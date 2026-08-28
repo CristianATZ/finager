d22ea3f9b7e00df9a90d7d3741dc8136b4806dbc
Cristian <s20120154@alumnos.itsur.edu.mx>
2026-08-28 00:13:23 -0600

    core: ui tokens

---
+++ core/ui/src/main/java/com/devtorres/ui/theme/Spacing.kt
@@ -0,0 +1,24 @@
+package com.devtorres.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Spacing(
+    val extraSmall: Dp = 4.dp,
+    val small: Dp = 8.dp,
+    val medium: Dp = 12.dp,
+    val default: Dp = 16.dp,
+    val large: Dp = 24.dp,
+    val extraLarge: Dp = 32.dp,
+    val extraExtraLarge: Dp = 40.dp,
+    val huge: Dp = 48.dp
+)
+
+val LocalSpacing = staticCompositionLocalOf { Spacing() }
+
+val MaterialTheme.spacing: Spacing
+    @Composable
+    get() = LocalSpacing.current
